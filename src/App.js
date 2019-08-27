224d5bd4adfec50b271bdea94df38698aee0f313
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-27 13:38:10 +0200

    Move state to App and pass as Props to PurrStream

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import PurrStream from './components/PurrStream';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <PurrStream />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      purrs: [
+          { author: 'AuthorA', content: 'Content of the purr 1' },
+          { author: 'AuthorB', content: 'Content of the purr 2' },
+          { author: 'AuthorC', content: 'Content of the purr 3' },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <PurrStream purrs={this.state.purrs} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/PurrStream.js
+++ src/components/PurrStream.js
@@ -2,22 +2,10 @@ import React, { Component } from 'react';
 import Purr from './Purr'
 
 class PurrStream extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            purrs: [
-                { author: 'AuthorA', content: 'Content of the purr 1' },
-                { author: 'AuthorB', content: 'Content of the purr 2' },
-                { author: 'AuthorC', content: 'Content of the purr 3' },
-            ]
-        }
-    }
-
     render() {
         return (
             <div className="cards">
-                { this.state.purrs.map((item, key) =>
+                { this.props.purrs.map((item, key) =>
                     <Purr author={item.author} content={item.content} />
                 )}
             </div>
