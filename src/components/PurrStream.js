db0a1e70b67b200b6031bf1a7468324263303ff9
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-27 13:04:33 +0200

    Render purr cards based on data in state

--- src/components/PurrStream.js
+++ src/components/PurrStream.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 
 class PurrStream extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            purrs: [
+                { author: 'AuthorA', content: 'Content of the purr 1' },
+                { author: 'AuthorB', content: 'Content of the purr 2' },
+                { author: 'AuthorC', content: 'Content of the purr 3' },
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="cards">
-                <div className="card">
-                    <div className="card-body">
-                        <h3 class="card-title">Author1</h3>
-                        <p class="card-text">Content of the purr 1</p>
+                { this.state.purrs.map((item, key) =>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 class="card-title">{item.author}</h3>
+                            <p class="card-text">{item.content}</p>
+                        </div>
                     </div>
-                </div>
-                <div className="card">
-                    <div className="card-body">
-                        <h3 class="card-title">Author1</h3>
-                        <p class="card-text">Content of the purr 2</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-body">
-                        <h3 class="card-title">Author 3</h3>
-                        <p class="card-text">Content of the purr 3</p>
-                    </div>
-                </div>
+                )}
             </div>
         )
     }
