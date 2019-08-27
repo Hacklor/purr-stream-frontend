7566519ca464192711b99433a65d6b7fc1a33d8e
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-27 22:33:10 +0200

    Creates instance of PurrStream

---
+++ src/components/PurrStream.js
@@ -0,0 +1,5 @@
+
+class PurrStream {
+}
+
+export default PurrStream;
---
+++ src/components/PurrStream.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PurrStream from './PurrStream';
+
+describe('PurrStream', () => {
+  it('creates instance', () => {
+    new PurrStream()
+  });
+});
