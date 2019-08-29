539a6332812be59088bd33530ef8fd4c0f249c08
Nicole Bekkers <hackybekkers@hacklor.com>
2019-08-29 14:36:44 +0200

    Changing the author and setting state

--- src/components/CreatePurr.js
+++ src/components/CreatePurr.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class CreatePurr extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        author: ''
+    }
+
+    this.handleAuthorChange = this.handleAuthorChange.bind(this)
+  }
+
+  handleAuthorChange() {
+    this.setState({author: event.target.value})
+  }
+
   render() {
     return (
-        <div />
+        <form>
+            <label>
+                Author:
+                <input type="text" value={this.state.author} onChange={this.handleAuthorChange} />
+            </label>
+        </form>
     )
   }
 }
--- src/components/CreatePurr.test.js
+++ src/components/CreatePurr.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 import CreatePurr from './CreatePurr';
 
 describe('CreatePurr', () => {
     it('renders an instance', () => {
         render(<CreatePurr />)
+    })
+
+    it('fills the form', () => {
+        const { getByLabelText } = render(<CreatePurr />)
+
+        fireEvent.change(getByLabelText('Author:'), { value: 'NewAuthor' })
+
+        wait(() => {
+            expect(getByLabelText('Author:').value).toEqual('NewAuthor')
+        })
     })
 })
